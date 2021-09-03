4762ecfd886a2cc4566ab317e06ebaa6424d268a
Raquel Leão <vc.raquel@gmail.com>
2021-09-03 11:05:26 -0300

    entrega mínima cadastra e mostra um funcionário

--- src/br/com/zup/desafio_modulo2.java
+++ src/br/com/zup/desafio_modulo2.java
@@ -1,5 +1,43 @@
 package br.com.zup;
+//Crie um programa para gerenciar uma lista de funcionários
+// de uma empresa, cada funcionário tem
+// nome, telefone, email e CPF.
+// No final do programa deve ser exibida
+// a lista de funcionários cadastrados.
+
+//Entrega Mínima: O sistema permite, via terminal,
+// inserir pelo menos 1 funcionário
+//em uma lista e retornar o mesmo na tela.
+
+//Entrega Média: O sistema permite adicionar mais de um
+// funcionário e apresenta um menu para decidir se deverá:
+//1. Adicionar mais funcionários.
+//2. Exibir a lista de todos os funcionários cadastrados.
+//3. Encerrar o programa.
+
+//Entrega Máxima: O Sistema permite excluir um funcionário usando como
+//parâmetro o CPF e não permite inserir um funcionário com o CPF repetido.
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class desafio_modulo2 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Map<String,String> funcionarios = new HashMap<String,String>();
 
+        System.out.println("Digite o nome Completo do Funcionário: ");
+        String nome = leitor.nextLine();
+        System.out.println("Digite o seu telefone: ");
+        String telefone = leitor.nextLine();
+        System.out.println("Digite o e-mail do funcionário: ");
+        String email = leitor.nextLine();
+        System.out.println("Digite o CPF do funcionário: ");
+        String cpf = leitor.nextLine();
+        funcionarios.put(cpf, "Nome: " + nome + " Telefone: " + telefone);
+
+        for (String referencia : funcionarios.keySet()){
+            System.out.println(funcionarios.get(referencia) + " CPF: " + referencia);
+        }
+    }
 }
